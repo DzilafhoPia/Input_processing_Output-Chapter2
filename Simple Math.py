a67332f57a9a9684f7f807188831a2d37ce18294
DzilafhoPia <dzilafhopia17@gmail.com>
2023-05-18 15:03:32 +0200

    "simple Math"

---
+++ Simple Math.py
@@ -0,0 +1,10 @@
+# program that prompts for two numbers. Print the sum, difference, product, and quotient of those numbers.
+
+print("What is the first Number? ")
+first_number = int(input())
+print("What is the second Number? ")
+second_number = int(input())
+print(first_number, "+", second_number, "=", first_number + second_number)
+print(first_number, "-", second_number, "=", first_number - second_number)
+print(first_number, "*", second_number, "=", first_number * second_number)
+print(first_number, "//", second_number, "=", first_number / second_number)
