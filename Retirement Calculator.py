fd8fb4f351d0ad9f72f2720a7a68fc0e6c31538b
DzilafhoPia <dzilafhopia17@gmail.com>
2023-06-19 13:55:26 +0200

    "Retirement Calculator"

--- Retirement Calculator.py
+++ Retirement Calculator.py
@@ -1,17 +1,15 @@
 # Create a program that determines how many years you have left until retirement and the year you can retire.
 
-print("What is your current age? ")
-current_age = int(input())
-print("At what age would you like to retire? ")
-retirement_age = int(input())
+current_age = int(input("What is your current age? "))
+retirement_age = int(input("At what age would you like to retire? "))
 number_of_years = int(retirement_age - current_age)
 print("What is the current year? ")
 import datetime
 today = datetime.date.today()
 current_year = today.year
 print(int(current_year))
-print("What is the retirement")
+print("What is the year of retirement")
 retirement_year = current_year + number_of_years
 print(int(retirement_year))
-print("you have " + str(number_of_years) + " years left until you can retire.")
-print("its " + str(current_year) + ", so you can retire in " + str(retirement_year)+".")
+print(f"you have {number_of_years} years left until you can retire.")
+print(f"its {current_year}, so you can retire in {retirement_year}.")
